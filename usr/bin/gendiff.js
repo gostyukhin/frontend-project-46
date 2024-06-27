95cf2e3ac13dc4f9361a292f97c5b0dac0865976
gostyukhin <evgeniy.gostyukhin.95@mail.ru>
2024-06-27 16:09:57 +0700

    update: gendiff file

--- usr/bin/gendiff.js
+++ usr/bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse();
