657b48a4f88794779e6b155778daf4e34fd181cd
gostyukhin <evgeniy.gostyukhin.95@mail.ru>
2024-06-28 20:08:49 +0700

    hexlet-project-46

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .argument('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
